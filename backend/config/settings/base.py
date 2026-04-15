6ccf76c6a5579e9afb784312421a8e3c6bff3761
Aleksandar Stanchev <altryx15@gmail.com>
2026-04-15 19:44:05 +0300

    fetch nightscout data issue fix

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.users',
+    'apps.businesses',
+    'apps.products',
+    'apps.sales',
+    'apps.forecasting',
 ]
 
 MIDDLEWARE = [
