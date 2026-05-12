93829775852ad5d1b45f912861bebf57a7de435b
Aleksandar Stanchev <altryx15@gmail.com>
2026-05-12 21:26:23 +0300

    forecast result model

--- backend/apps/forecasting/models.py
+++ backend/apps/forecasting/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from core.models import TimeStampedModel
 
-# Create your models here.
+class ForecastResult(TimeStampedModel):
+    class HorizonOptions(models.TextChoices):
+        ONE_WEEK = "7 days", "7 DAYS"
+        ONE_MONTH = "30 days", "30 DAYS"
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    predicted_quantity = models.IntegerField()
+    horizon = models.CharField(max_length=10, choices=HorizonOptions)
+    confidence_score = models.DecimalField(max_digits=7, decimal_places=4)
