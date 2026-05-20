0cd6880519ab65ab0e280804c31581605c4047e1
Aleksandar Stanchev <altryx15@gmail.com>
2026-05-20 16:29:20 +0300

    using custom user model with get_user_model() instead of django's built in User model.

--- backend/apps/users/serializers/registration_serializer.py
+++ backend/apps/users/serializers/registration_serializer.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from ..services import UserService
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('username', 'email', 'password', 'first_name', 'last_name')
         extra_kwargs = {"password": {"write_only": True}}
 
--- backend/apps/users/serializers/user_serializer.py
+++ backend/apps/users/serializers/user_serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('id', 'email', 'full_name', 'last_name')
