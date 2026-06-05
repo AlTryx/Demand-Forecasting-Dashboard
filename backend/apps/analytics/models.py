b3652040e3b4b6f11f27c1de6e58a0e29b2b37ba
Aleksandar Stanchev <altryx15@gmail.com>
2026-06-05 18:09:45 +0300

    inventory models

--- backend/apps/analytics/models.py
+++ backend/apps/analytics/models.py
@@ -6,11 +6,10 @@ class EvaluationMetric(TimeStampedModel):
         ACCURATE = "ACCURATE", "Accurate"
         OVER_FORECASTED = "OVER_FORECASTED", "Over Forecasted (Risk of Overstock)"
         UNDER_FORECASTED = "UNDER_FORECASTED", "Under Forecasted (Risk of Stockout)"
-
+        
     forecast_result = models.ForeignKey('forecasting.ForecastResult', on_delete=models.CASCADE)
     actual_quantity_sold = models.IntegerField()
     absolute_error = models.DecimalField(max_digits=5, decimal_places=2)
     percentage_error = models.DecimalField(max_digits=5, decimal_places=2)
     bias = models.IntegerField()
-    status = models.CharField(max_length=30, roles=ForecastStatus.choices, default=ForecastStatus.ACCURATE)
-    
+    status = models.CharField(max_length=30, choices=ForecastStatus.choices, default=ForecastStatus.ACCURATE)
--- backend/apps/inventory/models.py
+++ backend/apps/inventory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from core.models import TimeStampedModel
 
-# Create your models here.
+class Inventory(TimeStampedModel):
+    class InventoryStatus(models.TextChoices):
+        OK = "OK", "Ok"
+        OVERSTOCK = "OVERSTOCK", "Overstock"
+        STOCKOUT_RISK = "STOCKOUT_RISK", "STOCKOUT_RISK"
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    days_of_stock_left = models.IntegerField()
+    status = models.CharField(max_length=20, choices=InventoryStatus.choices, default=InventoryStatus.OK)
